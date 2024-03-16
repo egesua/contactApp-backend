d8891475e3b81c9d3302d31bba470a1cedb47f21
egesua <egesu06@gmail.com>
2024-03-16 19:03:00 +0300

    entity package and contact class created.

--- src/main/java/com/egesua/contactAppbackend/Application.java
+++ src/main/java/com/egesua/contactAppbackend/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ContactAppBackendApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ContactAppBackendApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
