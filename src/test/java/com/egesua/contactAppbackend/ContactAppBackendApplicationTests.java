f83ff663766eb5ec2237d22596a237d972627506
egesua <egesu06@gmail.com>
2024-03-16 18:55:09 +0300

    created the project backend.

---
+++ src/test/java/com/egesua/contactAppbackend/ContactAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egesua.contactAppbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
