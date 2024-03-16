d684aca61b3da7ea3fe7b90cba1e3e0db709df34
egesua <egesu06@gmail.com>
2024-03-16 19:06:15 +0300

    repository package created, contactRepository created.

---
+++ src/main/java/com/egesua/contactAppbackend/repository/ContactRepository.java
@@ -0,0 +1,12 @@
+package com.egesua.contactAppbackend.repository;
+
+import com.egesua.contactAppbackend.entity.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ContactRepository extends JpaRepository<Contact, String> {
+    Optional<Contact> findById(String id);
+}
