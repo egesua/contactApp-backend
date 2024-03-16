925434f640592627e78a19f5d615662f6a731ceb
egesua <egesu06@gmail.com>
2024-03-16 19:51:18 +0300

    service layer created, contact service completed.

---
+++ src/main/java/com/egesua/contactAppbackend/service/ContactService.java
@@ -0,0 +1,75 @@
+package com.egesua.contactAppbackend.service;
+
+import com.egesua.contactAppbackend.entity.Contact;
+import com.egesua.contactAppbackend.repository.ContactRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+import static com.egesua.contactAppbackend.constant.Constant.PHOTO_DIRECTORY;
+
+@Service
+@Slf4j
+@Transactional(rollbackOn = Exception.class)
+@RequiredArgsConstructor
+public class ContactService {
+
+    //dependency injection
+    private final ContactRepository contactRepository;
+
+    public Page<Contact> getAllContacts(int page, int size) {
+        return contactRepository.findAll(PageRequest.of(page, size, Sort.by("name")));
+    }
+
+    public Contact getContact(String id) {
+        return contactRepository.findById(id).orElseThrow(() -> new RuntimeException("Contact not found."));
+    }
+
+    public Contact createContact(Contact contact) {
+        return contactRepository.save(contact);
+    }
+
+    public void deleteContact(Contact contact) {
+        contactRepository.deleteById(contact.getId());
+    }
+
+    public String uploadPhoto(String id, MultipartFile file) {
+        Contact contact = getContact(id);
+        String photoUrl = photoFunction.apply(id, file);
+        contact.setPhotoUrl(photoUrl);
+        contactRepository.save(contact);
+        return photoUrl;
+    }
+
+    //biFunction usage, first two parameters goes in, returns as a third param.
+    private final Function<String, String> fileExtension = filename -> Optional.of(filename).filter(name -> name.contains("."))
+            .map(name -> "." + name.substring(filename.lastIndexOf(".") + 1)).orElse(".png");
+
+    private final BiFunction<String, MultipartFile, String> photoFunction = (id, image) -> {
+        String filename = id + fileExtension.apply(image.getOriginalFilename());
+        try {
+            Path fileStorageLocation = Paths.get(PHOTO_DIRECTORY).toAbsolutePath().normalize();
+            if(!Files.exists(fileStorageLocation)) { Files.createDirectories(fileStorageLocation); }
+            Files.copy(image.getInputStream(), fileStorageLocation.resolve(filename), REPLACE_EXISTING);
+            return ServletUriComponentsBuilder
+                    .fromCurrentContextPath()
+                    .path("/contacts/image/" + filename).toUriString();
+        }catch (Exception exception) {
+            throw new RuntimeException("Unable to save image");
+        }
+    };
+}
